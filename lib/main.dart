d5c4e2c4b3c7f20ca695742b1ffe9c51ff20e8f9
vitarahmada <vita.20003@mhs.unesa.ac.id>
2022-10-17 10:00:19 +0700

    tampilan halaman utama

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "CRUD Data Pelanggan Apotek",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("DATA PELANGGAN APOTEK"),
+      ),
+    );
+  }
+}
